8794d90a92beb0afe882d3815bfc9d228a138273
Dragan Bogatic <dbogaticE@hotmail.com>
2021-01-31 18:01:58 -0600

    priv keys

--- wallet.py
+++ wallet.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 mnemonic = os.getenv("MNEMONIC")
-command = './derive -g --mnemonic --coin=ETH --numderive=3 --cols=path,address,privkey,pubkey --format=json'
+command = './derive -g --mnemonic --coin=BTCTEST --numderive=3 --cols=path,address,privkey,pubkey --format=json'
 
 p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 output, err = p.communicate()
@@ -18,4 +18,4 @@ print(keys)
 from constants import * 
 
 keys_df = pd.DataFrame(keys)
-keys_df.to_csv("resources/eth_keys")
+keys_df.to_csv("resources/btc_test_keys")
